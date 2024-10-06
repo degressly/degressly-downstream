ae162c663afb0daef3f420a70f36c4be19559075
Daniyaal Khan <me@daniyaalkhan.com>
2024-10-06 12:50:47 +0530

    Added filtering for hop-by-hop filters

--- src/main/java/com/degressly/proxy/downstream/client/DownstreamClient.java
+++ src/main/java/com/degressly/proxy/downstream/client/DownstreamClient.java
@@ -9,6 +9,8 @@ import org.springframework.lang.Nullable;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -19,7 +21,10 @@ public class DownstreamClient {
 
 	private final Logger logger = LoggerFactory.getLogger(DownstreamClient.class);
 
-	private static final Set<String> headersToSkip = new HashSet<>(Arrays.asList("transfer-encoding"));
+	private static final Set<String> headersToSkip = Set.of("Accept-Encoding", "accept-encoding", "connection",
+			"accept", "Accept", "Connection", "content-length", "Content-Length", "transfer-encoding", "host", "Host",
+			"Transfer-Encoding", "Keep-Alive", "keep-alive", "Trailer", "trailer", "Upgrade", "upgrade",
+			"Proxy-Authorization", "proxy-authorization", "Proxy-Authenticate", "proxy-authenticate");
 
 	public static ResponseEntity getResponse(String host, HttpServletRequest httpServletRequest,
 			MultiValueMap<String, String> headers, MultiValueMap<String, String> params, String body) {
@@ -42,7 +47,7 @@ public class DownstreamClient {
 					response.getHeaders(), response.getBody());
 
 		}
-		catch (HttpClientErrorException e) {
+		catch (RestClientResponseException e) {
 			logger.info("Response for for url {}: Status: {} Headers: {}, Body: {}", finalUrl, e.getStatusCode(),
 					e.getResponseHeaders(), e.getResponseBodyAsString());
 			newHeaders = filterHeaders(e.getResponseHeaders());
@@ -54,7 +59,7 @@ public class DownstreamClient {
 
 	}
 
-	private static MultiValueMap<String, String> filterHeaders(@Nullable HttpHeaders headers) {
+	private static MultiValueMap<String, String> filterHeaders(@Nullable MultiValueMap<String, String> headers) {
 		if (Objects.isNull(headers)) {
 			return new LinkedMultiValueMap<>();
 		}
